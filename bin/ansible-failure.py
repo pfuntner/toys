bcfb3c299f36dec1bfcf308005aa15693b2086d3
John Pfuntner <jpfuntne@cisco.com>
2025-05-08 08:07:25 -0400

    Reimagining tool

--- bin/ansible-failure.py
+++ bin/ansible-failure.py
@@ -17,12 +17,19 @@ log.setLevel(logging.WARNING - (args.verbose or 0)*10)
 if sys.stdin.isatty():
   parser.error('stdin must be redirected')
 
-hits = re.findall(r'^[^{]*\sfatal:\s[^{]*({.*})$', sys.stdin.read(), flags=re.MULTILINE)
-log.info(f'{len(hits)=}')
-if not hits:
-  parser.error('No failure found')
+fatal_regexp = re.compile(r'failed|FAILED|fatal|FATAL')
+fatal_summary_regexp = re.compile(r'failed=\d')
 
-if len(hits) > 1:
-  parser.error('More than one failure')
+last_pos = None
+lines = sys.stdin.read().splitlines()
+for (pos, line) in enumerate(lines):
+  if fatal_regexp.search(line) and not fatal_summary_regexp.search(line):
+    last_pos = pos
 
-print(hits[0])
+log.info(f'last_pos = {last_pos}')
+if last_pos:
+  top = max(0, last_pos - 10)
+  bottom = min(len(lines), last_pos + 10)
+
+  for line in lines[top:bottom+1]:
+    print(line)
